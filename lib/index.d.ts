eb35976d5f805de44d89c499b2a180bc2366c3de
atricaud <a.tricaud@glowbl.com>
2019-03-04 11:24:33 +0100

    add typing

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,23 +1,39 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { MediaQuery as IMediaQuery } from "./MediaQuerySelector";
+import { StyleSheet, ViewStyle } from "react-native";
+// import { MediaQuery as IMediaQuery } from "./MediaQuerySelector";
 interface ResponsiveState {
     window: any;
+}
+
+type Orientation = "landscape" | "portrait";
+
+export interface IMediaQuery {
+    minHeight?: number;
+    maxHeight?: number;
+    minWidth?: number;
+    maxWidth?: number;
+    minAspectRatio?: number;
+    maxAspectRatio?: number;
+    minPixelRatio?: number;
+    maxPixelRatio?: number;
+    orientation?: Orientation;
+    condition?: boolean;
+    platform?: string;
 }
 
 interface MediaQueryProps extends IMediaQuery {
     children?: React.ReactElement<any>;
 }
 
+export interface MediaQueryStyle {
+    query: IMediaQuery;
+    style: any;
+}
+
 export class ResponsiveComponent extends React.Component {}
 
 export class ResponsiveStyleSheet {
-    static select(styles: MediaQueryStyle[]): StyleSheet.NamedStyles<any>;
-}
-
-export interface MediaQueryStyle {
-    query: MediaQuery<MediaQueryProps>;
-    style: StyleSheet.NamedStyles<any>;
+    static select(styles: MediaQueryStyle[]): any;
 }
 
 export class MediaQuery<MediaQueryProps> extends ResponsiveComponent {}
