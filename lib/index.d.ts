8f5ded74989ebfe5947c7cb00996f8eebe99982f
atricaud <a.tricaud@glowbl.com>
2019-04-17 09:49:11 +0200

    add typing for missing Device class

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,56 +1,72 @@
-import React from "react";
-import { StyleSheet, ViewStyle } from "react-native";
+import React from 'react';
+import { StyleSheet, ViewStyle } from 'react-native';
 
 interface ResponsiveState {
-    window: any;
+  window: any;
 }
 
-type Orientation = "landscape" | "portrait";
+type Orientation = 'landscape' | 'portrait';
 
 export interface IMediaQuery {
-    minHeight?: number;
-    maxHeight?: number;
-    minWidth?: number;
-    maxWidth?: number;
-    minAspectRatio?: number;
-    maxAspectRatio?: number;
-    minPixelRatio?: number;
-    maxPixelRatio?: number;
-    orientation?: Orientation;
-    condition?: boolean;
-    platform?: string;
+  minHeight?: number;
+  maxHeight?: number;
+  minWidth?: number;
+  maxWidth?: number;
+  minAspectRatio?: number;
+  maxAspectRatio?: number;
+  minPixelRatio?: number;
+  maxPixelRatio?: number;
+  orientation?: Orientation;
+  condition?: boolean;
+  platform?: string;
 }
 
 export interface MediaQueryStyle {
-    query: IMediaQuery;
-    style: StyleSheet.NamedStyles<any>;
+  query: IMediaQuery;
+  style: StyleSheet.NamedStyles<any>;
 }
 
 export interface MediaQueryProps extends IMediaQuery {
-    children?: React.ReactElement<any>;
+  children?: React.ReactElement<any>;
 }
 
 interface Dimensions {
-    width: number;
-    height: number;
-    scale: number;
-    fontScale: number;
+  width: number;
+  height: number;
+  scale: number;
+  fontScale: number;
 }
 
 interface AllDimensions {
-    window: Dimensions;
+  window: Dimensions;
+}
+
+interface DimensionChangeSubscription {
+  unsubscribe(): void;
 }
 
 declare class ResponsiveComponent<
-    P = {},
-    S = AllDimensions
+  P = {},
+  S = AllDimensions
 > extends React.Component<P, S> {}
 
 declare class ResponsiveStyleSheet {
-    static select(styles: MediaQueryStyle[]): any;
+  static select(styles: MediaQueryStyle[]): any;
 }
 
 declare class MediaQuery extends ResponsiveComponent<MediaQueryProps> {}
 
-export { ResponsiveComponent, ResponsiveStyleSheet, MediaQuery,
-    AllDimensions as ResponsiveComponentState };
+declare class Device {
+  static subscribeToDimensionChanges(
+    handler: (Object) => void
+  ): DimensionChangeSubscription;
+  static dimensions(): AllDimensions;
+}
+
+export {
+  ResponsiveComponent,
+  ResponsiveStyleSheet,
+  MediaQuery,
+  Device,
+  DimensionChangeSubscription
+};
